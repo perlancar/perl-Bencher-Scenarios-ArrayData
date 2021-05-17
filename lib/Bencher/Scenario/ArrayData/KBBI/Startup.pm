31e76d255b26c223d0a3bdd1917f013e1583d5db
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-05-17 23:35:33 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/ArrayData/KBBI/Startup.pm
@@ -0,0 +1,33 @@
+package Bencher::Scenario::ArrayData::KBBI::Startup;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $scenario = {
+    summary => 'Benchmark startup of ArrayData::Word::ID::KBBI vs WordList::ID::KBBI',
+    module_startup => 1,
+    modules => {
+    },
+    participants => [
+        {module=>'ArrayData::Word::ID::KBBI'},
+        {module=>'WordList::ID::KBBI'},
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 BENCHMARK NOTES
+
+The startup overhead is fairly close. L<WordList> modules used to have
+significantly smaller overhead before the use of roles. But L<Role::Tiny>'s
+overhead is still tiny (~2-3ms on my laptop) so startup overhead should not be
+an issue in most cases.
+
+=head1 SEE ALSO
---
+++ lib/Bencher/Scenarios/ArrayData.pm
@@ -0,0 +1,9 @@
+package Bencher::Scenarios::ArrayData;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Scenarios related to ArrayData
